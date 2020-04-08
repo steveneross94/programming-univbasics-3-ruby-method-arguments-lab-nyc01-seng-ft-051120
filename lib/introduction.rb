6189607ae7afedf4920b15259d1caffd7354ab6b
Steven Ross <ross.steven@me.com>
2020-04-08 17:49:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ def introduction_with_language(name, language)
   puts "hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language(name, language="Ruby")
+def introduction_with_language_optional(name, language="Ruby")
   puts "hi, my name is #{name} and I am learning to program in #{language}."
 end
 
